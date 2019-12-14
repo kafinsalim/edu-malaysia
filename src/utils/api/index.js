838665d7da35185cc0c071996ae157942dd0a35a
Kafin Salim <kafinsalim@gmail.com>
2019-12-15 02:15:17 +0700

    chore: change api

--- src/utils/api/index.js
+++ src/utils/api/index.js
@@ -1,5 +1,6 @@
 import axios from "axios";
 
-const base_url = "http://app-13a41883-30f6-425f-b5f7-7264fc31a1e7.cleverapps.io";
+const base_url =
+  "https://app-13a41883-30f6-425f-b5f7-7264fc31a1e7.cleverapps.io";
 
 export { base_url };
